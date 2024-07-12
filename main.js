1191253eb819d1d964d2c65da4590985a160bc6b
salohiddindev1 <169024491+salohiddindev1@users.noreply.github.com>
2024-07-12 15:10:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting:");
+let spaceId = prompt("Space ID ni kiriting:");
+let spacePassword = prompt("Space parolingizni kiriting:");
+
+let object = {
+  ism: ism,
+  spaceId: spaceId,
+  spacePassword: spacePassword,
+};
+
+console.log(object);
